57b13dadb09c025adc89f955e39b4377371361cc
Cuffuffles <cuffuffles@gmail.com>
2020-03-26 18:29:48 +1000

    Added improved FPS limiter

--- js/bundle.js
+++ js/bundle.js
@@ -1,16 +1,79 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const io = require("socket.io-client");
 const socket = io("http://localhost:8081");
+var _rAF;
 
 function init() {
     console.log("init");
+    _rAF = window.requestAnimFrame;
     addExit();
     watermark();
     fixLinks();
+    initFpsLimiter();
+    initWindows();
 
     //Tell main we're done with preload
     socket.emit("preloaded");
     socket.close();
+}
+
+function initFpsLimiter() {
+    if(getCXSettings('fpsLimit') === null || !(Number.isInteger(Number.parseInt(getCXSettings('fpsLimit'))))) setCXSettings('fpsLimit', '0');
+    if(getCXSettings('fpsLimit') < 10 && getCXSettings('fpsLimit') != 0) setCXSettings('fpsLimit', '10');
+    fpsLimit();
+}
+
+function updateFps(fps) {
+    fps = parseInt(fps);
+    if(fps > 1000) fps = 1000;
+    if(fps < 10 && fps != 0) fps = 10;
+    if(!(Number.isInteger(fps))) fps = 0;
+    setCXSettings('fpsLimit', fps);
+    document.getElementById("slid_fps").value = fps;
+    document.getElementById("box_fps").value = fps;
+    fpsLimit();
+}
+
+function fpsLimit() {
+    if(getCXSettings('fpsLimit') === null || !(Number.isInteger(Number.parseInt(getCXSettings('fpsLimit'))))) setCXSettings('fpsLimit', '0');
+    if(getCXSettings('fpsLimit') < 10 && getCXSettings('fpsLimit') != 0) setCXSettings('fpsLimit', '10');
+    var start = 0;
+    var fps = getCXSettings('fpsLimit');
+    if(fps == '0') {
+        console.log('no limit');
+        window.requestAnimFrame = _rAF;
+    } else {
+        console.log('fps capped at: ' + fps);
+        var fpsInterval = 1000 / fps;
+        window.requestAnimFrame = function(...args) {
+            for (var i = 1e99; i > 0; i--) {
+                if (window.performance.now() - start > fpsInterval) {
+                    break;
+                }
+            }
+            start = window.performance.now();
+            _rAF(args[0]);
+        }
+    }
+}
+
+function initWindows() {
+    var _settingsGen = windows[0].getCSettings;
+    windows[0].getCSettings = function() {
+        var tempHTML = _settingsGen();
+        setTimeout(() => {
+            var divList = menuWindow.getElementsByClassName('settName');
+            $.each(divList, (index, value) => {
+                var tempString = value.innerHTML;
+                if(tempString.startsWith('Frame Cap')) {
+                    value.innerHTML = '<div class="settName" id="fps_div" style="display:block">FPS Limit<input type="number" class="sliderVal" id="slid_fps" min="0" max="1000" value="' + getCXSettings('fpsLimit') + '" style="border-width:0px"><div class="slidecontainer"><input type="range" id="box_fps" min="0" max="1000" step="1" value="' + getCXSettings('fpsLimit') + '" class="sliderM"></div></div>';
+                }
+            });
+            slid_fps.addEventListener('input', () => { updateFps(slid_fps.value); });
+            box_fps.addEventListener('input', () => { updateFps(box_fps.value); });
+        }, 10);
+        return tempHTML;
+    }
 }
 
 function addExit() {
@@ -33,6 +96,18 @@ function fixLinks() {
         event.preventDefault();
         window.open(this.href);
     });
+}
+
+function setCXSettings(key, value) {
+    localStorage.setItem('cx' + key, value);
+}
+
+function getCXSettings(key) {
+    return localStorage.getItem('cx' + key);
+}
+
+function getClassIndex() {
+    return localStorage.getItem('classindex');
 }
 
 function isDefined() {
