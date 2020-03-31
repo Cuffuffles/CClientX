e91108f017d7a2fef1bc91001da0f9ea308f786b
Cuffuffles <cuffuffles@gmail.com>
2020-04-01 04:11:25 +1000

    removed copy link button from social profile - share button achieves the same thing

--- js/client.js
+++ js/client.js
@@ -1,6 +1,6 @@
 const io = require("socket.io-client");
 const socket = io("http://localhost:8081");
-var versionNum = "1.0.4";
+var versionNum = "1.0.5";
 
 function init() {
     console.log("init");
--- js/social.js
+++ js/social.js
@@ -1,12 +1,8 @@
-const loadObserver = new MutationObserver(insertButton);
 const donateObserver = new MutationObserver(donate);
 const config = { attributes: true };
 
 document.addEventListener('DOMContentLoaded', () => {
-    var url = window.location.href;
-    if(url.startsWith('https://krunker.io/social.html?p=profile&q=')) {
-        loadObserver.observe(loadMessage, config);
-    }else if(location.hash == "#donate") {
+    if(location.hash == "#donate") {
         updateWindow("profile");
         donateObserver.observe(loadMessage, config);
     }
@@ -16,15 +12,4 @@ function donate() {
     setTimeout(() => {
         giftPopup("Cuffuffles");
     }, 500);
-}
-
-function insertButton() {
-    var buttonHtml = '<div class="button profile buttonG" id="copyBtn">Copy</div>';
-    leftBtns.insertAdjacentHTML('beforeend', buttonHtml);
-    copyBtn.addEventListener('click', () => {
-        navigator.clipboard.writeText(window.location.href);
-        var popHtml = '<div style="color:#fff;font-size:20px;position:relative;margin-bottom:10px;text-align:center;">Profile Link Copied!</div>';
-        giftPopup();
-        giftPop.innerHTML = popHtml;
-    });
 }
